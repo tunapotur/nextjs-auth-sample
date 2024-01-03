79b3018ab366db0e993bb9822a2d5491599d35ff
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-01-03 21:15:19 +0300

    Authentication

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,20 +1,66 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (res.error) {
+        setError('Invalid Credentials');
+        return;
+      }
+
+      router.replace('dashboard');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="grid h-screen place-items-center">
       <div className="rounded-lg border-t-4 border-green-400 p-5 shadow-lg">
         <h1 className="my-4 text-xl font-bold">Login</h1>
 
-        <form className="flex flex-col gap-3">
-          <input className="input" type="text" placeholder="Email" />
-          <input className="input" type="password" placeholder="Password" />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="input"
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            type="password"
+            placeholder="Password"
+          />
           <button className="cursor-pointer bg-green-600 px-6 py-2 font-bold text-white">
             Login
           </button>
-          <div className="mt-2 w-fit rounded-md bg-red-500 px-3 py-1 text-sm text-white">
-            Error message
-          </div>
+
+          {error && (
+            <div className="mt-2 w-fit rounded-md bg-red-500 px-3 py-1 text-sm text-white">
+              {error}
+            </div>
+          )}
+
           <Link className="mt-3 text-right text-sm" href={'/register'}>
             Don&apos;t have an account?{' '}
             <span className="underline">Register</span>
--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,14 +1,24 @@
+'use client';
+
+import { signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
+
 export default function UserInfo() {
+  const { data: session } = useSession();
+
   return (
     <div className="grid h-screen place-items-center">
       <div className="bg-zince-300/10 my-6 flex flex-col gap-2 p-8 shadow-lg">
         <div>
-          Name: <span className="font-bold">John</span>
+          Name: <span className="font-bold">{session?.user?.name}</span>
         </div>
         <div>
-          Email: <span className="font-bold">john@gmail.com</span>
+          Email: <span className="font-bold">{session?.user?.email}</span>
         </div>
-        <button className="mt-3 bg-red-500 px-6 py-2 font-bold text-white">
+        <button
+          onClick={() => signOut()}
+          className="mt-3 bg-red-500 px-6 py-2 font-bold text-white"
+        >
           Log Out
         </button>
       </div>
