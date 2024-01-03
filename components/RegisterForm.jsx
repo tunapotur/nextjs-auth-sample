33b42bf5de840a3327877e1c3addb256af34abb1
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-01-03 15:46:40 +0300

    simple api

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,22 +1,79 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function RegisterForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setError('All fields are necessary.');
+      return;
+    }
+
+    try {
+      const res = await fetch('api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      if (res.ok) {
+        const form = e.target;
+        form.reset();
+      } else {
+        console.log('User registration failed.');
+      }
+    } catch (error) {
+      console.log('Error during registration: ', error);
+    }
+  };
+
   return (
     <div className="grid h-screen place-items-center">
       <div className="rounded-lg border-t-4 border-green-400 p-5 shadow-lg">
         <h1 className="my-4 text-xl font-bold">Register</h1>
 
-        <form className="flex flex-col gap-3">
-          <input className="input" type="text" placeholder="Full Name" />
-          <input className="input" type="text" placeholder="Email" />
-          <input className="input" type="password" placeholder="Password" />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            className="input"
+            type="text"
+            placeholder="Full Name"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="input"
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            type="password"
+            placeholder="Password"
+          />
           <button className="cursor-pointer bg-green-600 px-6 py-2 font-bold text-white">
             Register
           </button>
 
-          <div className="mt-2 w-fit rounded-md bg-red-500 px-3 py-1 text-sm text-white">
-            Error message
-          </div>
+          {error && (
+            <div className="mt-2 w-fit rounded-md bg-red-500 px-3 py-1 text-sm text-white">
+              {error}
+            </div>
+          )}
 
           <Link className="mt-3 text-right text-sm" href={'/'}>
             Already have an account? <span className="underline">Login</span>
